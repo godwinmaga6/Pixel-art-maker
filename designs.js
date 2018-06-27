3164b981ca6a8a7bd8aea45c5d44eaf1be0e4952
godwinmaga6 <40617575+godwinmaga6@users.noreply.github.com>
2018-06-27 09:14:55 +0100

    Modify starter code
    Complete project of Pixel Art Maker

---
+++ designs.js
@@ -0,0 +1,33 @@
+// Select color input
+// Select size input
+var heigth, width, color;
+// When size is submitted by the user, call makeGrid()
+$('#sizePicker').submit(function (event) {
+	event.preventDefault();
+	height = $('#inputHeight').val();
+	width = $('#inputWidth').val();
+	makeGrid(height, width);
+});
+
+
+function makeGrid(x, y) {
+	$('tr').remove();
+
+// Your code goes here!
+	for (var i = 1; i <= x; i++) {
+		$('#pixelCanvas').append('<tr id=table' + i + '></tr>');
+		for(var n = 1; n <= y; n++) {
+			$('#table' + i).append('<td></td>');
+		}
+	}
+
+	$('td').click(function addColor(){
+		color = $('#colorPicker').val();
+
+		if ($(this).attr('style')) {
+			$(this).removeAttr('style')
+		} else{
+			$(this).attr('style', 'background-color:' + color);
+		}
+	});
+}
